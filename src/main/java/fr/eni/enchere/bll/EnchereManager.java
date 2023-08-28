ee0a9c3bcb4c7106d27c01f5bd42079e1e08354f
Yanis GIBOIRE <yanis.giboire2022@campus-eni.fr>
2023-08-28 16:22:24 +0200

    add bll

---
+++ src/main/java/fr/eni/enchere/bll/EnchereManager.java
@@ -0,0 +1,27 @@
+package fr.eni.enchere.bll;
+
+import java.util.List;
+
+import fr.eni.enchere.bo.ArticleVendu;
+import fr.eni.enchere.bo.Categorie;
+import fr.eni.enchere.bo.Enchere;
+import fr.eni.enchere.bo.Retrait;
+import fr.eni.enchere.bo.Utilisateur;
+
+public interface EnchereManager {
+
+	public void addArticle(ArticleVendu articleVendu) throws DALException;
+	public List<ArticleVendu> getAllArticleVendu() throws DALException;
+	
+	public void addCategorie(Categorie categorie) throws DALException;
+	public List<Categorie> getAllCategorie() throws DALException;
+	
+	public void addEnchere(Enchere enchere) throws DALException;
+	public List<Enchere> getAllEnchere() throws DALException;
+	
+	public void addUtilisateur(Utilisateur utilisateur) throws DALException;
+	public List<Utilisateur> getAllUtilisateur() throws DALException;
+	
+	public void addRetrait(Retrait retrait) throws DALException;
+	public List<Retrait> getAllRetrait() throws DALException;
+}
---
+++ src/main/java/fr/eni/enchere/bll/EnchereManagerImpl.java
@@ -0,0 +1,112 @@
+package fr.eni.enchere.bll;
+
+import java.awt.Taskbar;
+import java.util.List;
+
+import fr.eni.enchere.bo.ArticleVendu;
+import fr.eni.enchere.bo.Categorie;
+import fr.eni.enchere.bo.Enchere;
+import fr.eni.enchere.bo.Retrait;
+import fr.eni.enchere.bo.Utilisateur;
+import fr.eni.enchere.dal.DAOFact;
+
+public class EnchereManagerImpl implements EnchereManager{
+
+	private Art
+	@Override
+	public void insert(ArticleVendu articleVendu) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ArticleVendu> getAll() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(Categorie categorie) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insert(Enchere enchere) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insert(Utilisateur utilisateur) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insert(Retrait retrait) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addArticle(ArticleVendu articleVendu) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ArticleVendu> getAllArticleVendu() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addCategorie(Categorie categorie) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Categorie> getAllCategorie() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addEnchere(Enchere enchere) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Enchere> getAllEnchere() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addUtilisateur(Utilisateur utilisateur) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Utilisateur> getAllUtilisateur() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addRetrait(Retrait retrait) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Retrait> getAllRetrait() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
