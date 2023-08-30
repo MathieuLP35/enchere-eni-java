9bab842773d98fdd6728b56bb3e04184f3bbe36f
mlepetitcorps2022 <mlepetitcorps2022@44SE34-DSV6YW2.ad.campus-eni.fr>
2023-08-30 13:54:19 +0200

    security: session max time (5 minutes)

--- src/main/java/fr/eni/right/ihm/login/LoginServlet.java
+++ src/main/java/fr/eni/right/ihm/login/LoginServlet.java
@@ -43,6 +43,8 @@ public class LoginServlet extends HttpServlet {
             // On met l'utilisateur en session
             request.getSession().setAttribute("user", user);
             
+            request.getSession().setMaxInactiveInterval(300);
+            
             request.getRequestDispatcher("/WEB-INF/home/accueil.jsp").forward(request, response);
         }
         
